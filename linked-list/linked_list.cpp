81621a499ada7a2577499c59f823b821f9c0af99
Shrey Bhardwaj <shrey.bhardwaj@juspay.in>
2023-04-27 23:38:16 +0530

    functions completed for linked-lists

--- linked-list/linked_list.cpp
+++ linked-list/linked_list.cpp
@@ -27,7 +27,7 @@ void linked_list::iter_seq() {
         printf("%d -> ", temp->get_data());
         temp = temp->next;
     }
-    printf("NULL\n");
+    printf("()\n");
     printf("Length of list is: %d\n", this->len());
 }
 
@@ -89,6 +89,78 @@ int linked_list::get_at(int i) {
     return temp->get_data();
 }
 
+void linked_list::set_at(int i, int x) {
+    if (i >= this->len()) {
+        fprintf(stderr, "Index %d falls outside length %d of the list. Cannot set element...\n", i, this->len());
+        return;
+    }
+    node* temp = head;
+    while (i > 0) {
+        temp = temp->next;
+        i--;
+    }
+    temp->set_data(x);
+}
+
+void linked_list::insert_at_end(int val) {
+    node* temp = new node(val);
+    if (tail != NULL) {
+        tail->next = temp;
+    }
+    tail = temp;
+    increase_len(1);
+
+    if (head == NULL) {
+        head = temp;
+    }
+}
+
+void linked_list::insert_at(int i, int x) {
+    if (i >= this->len()) {
+        this->insert_at_end(x);
+        return;
+    } else if (i == 0) {
+        this->insert_at_beginning(x);
+        return;
+    }
+    node* new_node = new node(x);
+    node* temp = head;
+
+    while (i > 1) {
+        temp = temp->next;
+        i--;
+    }
+
+    new_node->next = temp->next;
+    temp->next = new_node;
+    this->increase_len(1);
+}
+
+int linked_list::delete_at(int i) {
+    if (i >= this->len()) {
+        fprintf(stderr, "Index %d is out of bounds for length %d...\n", i, this->len());
+        return -1;
+    } else if (this->len() == 0) {
+        fprintf(stderr, "Cannot delete anything from an empty list innit...\n");
+        return -1;
+    } else if (i == 0) {
+        return this->delete_at_beginning();
+    }
+
+    node* temp = head;
+    while (i > 1) {
+        temp = temp->next;
+        i--;
+    }
+    int deleted_val = temp->next->get_data();
+    if (temp->next == tail) {
+        tail = temp;
+    }
+    temp->next = temp->next->next;
+    this->increase_len(-1);
+    return deleted_val;
+}
+
 void construct_list(linked_list* ll, int n) {
     for(int i = n; i >= 0; i--) {
         ll->insert_at_beginning(i);
@@ -99,18 +171,15 @@ int main() {
     printf("Driver function for linked-lists.\n");
 
     linked_list* ll1 = new linked_list();
-    construct_list(ll1, 15);
+    ll1->delete_at(0);
+    // construct_list(ll1, 15);
+    // ll1->insert_at_end(69);
+    // ll1->iter_seq();
+    // ll1->insert_at(10, 100);
+    // ll1->insert_at(0, 101);
+    // ll1->iter_seq();
+    ll1->delete_at(11);
     ll1->iter_seq();
-    ll1->get_at(1);
-    ll1->get_at(5);
-    ll1->get_at(8);
-    printf("%d\n", ll1->get_at(12));
-    ll1->get_at(15);
-    ll1->get_at(16);
-    ll1->get_at(20);
-    ll1->delete_at_beginning();
-    ll1->iter_seq();
-    printf("%d\n", ll1->get_at(12));
-    ll1->get_at(15);
+
     return 0;
 }
